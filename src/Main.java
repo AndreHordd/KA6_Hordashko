96088ede27b6fcfc3ecff8071e114f7d42c4240c
Andre <hordashko.andrii.clg@chnu.edu.ua>
2024-03-08 14:31:33 +0200

    Добавив конвертацію у бінарні числа й пітримку відємних чисел

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-         Прочитати з stdin N десяткових чисел, розділених пробілом чи новим рядком до появи EOF (макс довжина рядка 255 символів), кількість чисел може до 10000.
-         Рядки розділяються АБО послідовністю байтів 0x0D та 0x0A (CR LF), або одним символом - 0x0D чи 0x0A.
-         */
         Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
+        List<String> binaryNumbers = new ArrayList<>();
 
         while (scanner.hasNextInt()) {
             int number = scanner.nextInt();
             number = Math.max(Short.MIN_VALUE, Math.min(Short.MAX_VALUE, number));
-            numbers.add(number);
+            String binary = Integer.toBinaryString(0xFFFF & number);
+            while (binary.length() < 16) {
+                binary = "0" + binary;
+            }
+            binaryNumbers.add(binary);
         }
 
+        for (String binary : binaryNumbers) {
+            System.out.println(binary);
+        }
     }
 }
-
